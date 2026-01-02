ad252ff24cf878613bd41021e516a361a45ace61
Wahyudi <wahyuyudi2801@gmail.com>
2026-01-02 11:24:38 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+});
